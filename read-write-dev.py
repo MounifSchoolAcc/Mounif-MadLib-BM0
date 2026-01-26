9686d3968ba4fa910d31f892a140cd54cda0b04a
MounifSchoolAcc <mounifa@nycstudents.net>
2026-01-26 16:34:55 -0500

    save/edit

--- read-write-dev.py
+++ read-write-dev.py
@@ -3,34 +3,62 @@ from tkmacosx import Button
 
 import random
 
-def readFile():
+def editFile():
 	
 	with open("demofile.txt","r") as f:
 		content=f.read()
-		print(content)
-
+		tf.insert('1.0',content)
 def writeFile():
 	
 	with open("demofile.txt","w") as f:
-		f.write("text in txt file")
+		content=tf.get("1.0", 'end-1c')
+		f.write(content)
+
+def clearText():
+	tf.delete('1.0','end')
 
 root=Tk()
-root.title='test'
+root.title='main'
 root['background']='#3E4149'
 
-tf=Text(root)
+tf=Text(root,borderwidth=0)
 tf['background']='#3E4149'
-tf['width']=10
-tf['height']=5
+tf['fg']='#FFFDD0'
+tf['width']=25
+tf['height']=15
 
-readButton=Button(root,text='read file',command=readFile)
+clearButton=Button(
+	root,
+	text='clear text',
+	activebackground='#FFFFFF',
+	command=clearText)
 
-writeButton=Button(root,text='write file',command=writeFile)
+clearButton['fg']='#FFFDD0'
+clearButton['background']='#3E4149'
 
 
+editButton=Button(
+	root,
+	text='edit file',
+	activebackground='#FFFFFF',
+	command=editFile)
 
-tf.grid(column=0,row=0)
-readButton.grid(column=0,row=1)
-writeButton.grid(column=0,row=2)
+editButton['fg']='#FFFDD0'
+editButton['background']='#3E4149'
+
+
+writeButton=Button(
+	root,
+	text='write file',
+	activebackground='#FFFFFF',
+	command=writeFile)
+
+writeButton['background']='#3E4149'
+writeButton['fg']='#FFFDD0'
+
+tf.grid(column=1,row=0,rowspan=5,columnspan=5)
+editButton.grid(column=0,row=0)
+writeButton.grid(column=0,row=1)
+clearButton.grid(column=0,row=2)
 
 root.mainloop()
